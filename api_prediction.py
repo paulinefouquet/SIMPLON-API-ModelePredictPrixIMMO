3e2a9f9a4cd418bef40ada3da0bf743750b6852f
Pauline01~ <pauline.fouquet.dev@hotmail.com>
2024-01-17 13:42:34 +0100

    rajout de la version 0 sur PARIS 4P dans l'API

--- api_prediction.py
+++ api_prediction.py
@@ -4,16 +4,30 @@ import numpy as np
 
 app2 = FastAPI()
 
-with open(r'C:\Users\Utilisateur\Desktop\Dev IA\Semaine 03bis\Simplon-MODELE_PREDICTION\prediction_price_model.pkl', 'rb') as model_file:
+with open(r'prediction_price_model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
 
-@app2.post("/predict-model", description = 'Retourne un prix m2 moyen en Idf via un modèle entrainé en 2022' )
-def prediction(longitude: float, latitude: float, n_pieces: int, code_postal: int):
+@app2.post("/predict-model/V1", description = 'Retourne un prix m2 moyen en Idf via un modèle entrainé en 2022' )
+def prediction_Idf(longitude: float, latitude: float, n_pieces: int, code_postal: int):
     
     try:
-        #transformation des paramètres en un objet valide pour le modele
         coordonnees = np.array([[latitude, longitude, n_pieces, code_postal]])
         result = model.predict(coordonnees)
+        return {"prix_m2_moyen": result[0]}
+    
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Erreur{str(e)}")
+    
+
+with open(r'decision_tree_model.pkl', 'rb') as model_file:
+    model0 = pickle.load(model_file)
+
+@app2.post("/predict-model/V0", description = 'Retourne un prix m2 moyen via un modèle entrainé à Paris pour un 4 pièces en 2022' )
+def prediction_Paris4P(latitude: float, longitude: float):
+    
+    try:
+        coordonnees = np.array([[latitude, longitude]])
+        result = model0.predict(coordonnees)
         return {"prix_m2_moyen": result[0]}
     
     except Exception as e:
